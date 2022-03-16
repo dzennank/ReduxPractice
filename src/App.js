a9616ecae0dcebb8c476d7ea0f7d279800a208de
dzenan <dzenankrlic@gmail.com>
2022-03-16 16:01:24 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+
 
 function App() {
+
+const counter = useSelector((state) => state.counter)
+const dispatch = useDispatch();
+const incrementNumber = () => {
+  dispatch({ type: 'BTTN' });
+}
+
+const decrementNumber = () => {
+  dispatch({ type: 'DEC' });
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {counter}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={incrementNumber}>Increment number</button>
+        <button onClick={decrementNumber} className={'dec'}>Decrement number</button>
+        
       </header>
     </div>
   );
